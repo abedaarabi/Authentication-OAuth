9fe2d686f1828ddac323f448723b90829bd33853
abed <abedaarabi@gmail.com>
2020-07-24 13:52:16 +0200

    add 3-legged

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const axios = require("axios");
+const querystring = require("querystring");
+const app = express();
+const router = express.Router();
+const fs = require("fs");
+const dotenv = require("dotenv");
+const result = dotenv.config();
+const { forge_Id, client_secret } = process.env;
+function storeToken(data) {
+  fs.writeFileSync(__dirname + "/token.txt", JSON.stringify(data));
+}
+
+function getStoredToken() {
+  const result = fs.readFileSync(__dirname + "/token.txt");
+  return JSON.parse(result.toString());
+}
+
+setInterval(() => {
+  try {
+    const data = getStoredToken();
+
+    console.log(data.refresh_token);
+  } catch (e) {
+    console.log(e, "no token yet");
+  }
+}, 10 * 1000);
+
+router.get("/token/oauth/callback", async (req, res) => {
+  console.log(req.query.code);
+  const code = req.query.code;
+
+  const result = await authorize(code);
+  storeToken(result.data);
+
+  console.log(result.data);
+
+  res.send("hello");
+});
+
+router.get("/login", (req, res) => {
+  res.sendfile(__dirname + "/index.html");
+  console.log("Abed");
+});
+
+async function authorize(code) {
+  const url = `https://developer.api.autodesk.com/authentication/v1/gettoken`;
+  return axios({
+    method: "post",
+    url,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: querystring.stringify({
+      client_id: `${forge_Id}`,
+      client_secret: `${client_secret}`,
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: "http://2860737f1c76.ngrok.io/token/oauth/callback",
+    }),
+  });
+}
+async function refreshToken(token) {
+  const url = `https://developer.api.autodesk.com/authentication/v1/gettoken`;
+  return axios({
+    method: "post",
+    url,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: querystring.stringify({
+      client_id: `${forge_Id}`,
+      client_secret: `${client_secret}`,
+      grant_type: "refresh_token",
+      refresh_token: token,
+    }),
+  });
+}
+// authorize();
+app.use(router);
+const stratServer = function (PORT) {
+  app.listen(PORT, console.log(`server is running on ${PORT} ` || 8080));
+};
+
+stratServer(9090);
---
+++ readme.md
@@ -0,0 +1,3 @@
+Explore Forge APIs and services
+
+Forge tools help companies build applications that tap into their design and engineering data. From automated processes to alerts. 3D in-browser to virtual reality. Forge APIs can be combined with existing software systems to innovate how you work and get more from your data.
